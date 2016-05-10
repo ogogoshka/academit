876b4f0adc0613322dd66c879e099ece3b774f2b
ogogoshka <xxxpiracy@gmail.com>
2016-05-10 12:02:29 +0600

    создали Hello World

--- GeometricFigures/src/MainGeometricFigures.java
+++ GeometricFigures/src/MainGeometricFigures.java
@@ -4,48 +4,13 @@ public class MainGeometricFigures {
 
     public static void main(String[] args) {
 
-/*        Shapes.Square square = new Shapes.Square(5);
-        System.out.println(square.getArea());
-
-        square.setWidth(-10);
-        square.setHeight(4);
-
-        square.getHeight();
-        square.getWidth();
-
-        System.out.println(square.getHeight());
-        System.out.println(square.getWidth());
-        System.out.println(square.getArea());
-
-
-        Shapes.Rectangle rectangle = new Shapes.Rectangle(5, 6);
-        System.out.println(rectangle.getArea());
-
-        rectangle.setHeight(5);
-        rectangle.setWidth(7);
-
-        System.out.println(rectangle.getHeight());
-        System.out.println(rectangle.getWidth());
-        System.out.println(rectangle.getArea());
-
-
-        Shapes.Circle circle = new Shapes.Circle(6);
-        System.out.println(circle.getArea());
-
-        circle.setHeight(5);
-        System.out.println(circle.getHeight());
-
-        circle.setWidth(7);
-        System.out.println(circle.getWidth());
-
-        System.out.println(circle.getArea());
-        */
-
         Triangle triangle = new Triangle(2, 1, 2, 5, 6, 1);
         System.out.println(triangle.getArea());
 
         System.out.println(triangle.getWidth());
         System.out.println(triangle.getHeight());
+
+        Triangle triangle2 = new Triangle(2, 1, 3, 1, 6, 1);
 
     }
 }
--- GeometricFigures/src/Shapes/Triangle.java
+++ GeometricFigures/src/Shapes/Triangle.java
@@ -9,17 +9,22 @@ public class Triangle extends Shape {
     private double y3;
 
     public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
-        this.x1 = x1;
-        this.y1 = y1;
-
-        this.x2 = x2;
-        this.y2 = y2;
-
-        this.x3 = x3;
-        this.y3 = y3;
+        double a = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        double b = Math.sqrt(Math.pow((x3 - x2), 2) + Math.pow((y3 - y2), 2));
+        double c = Math.sqrt(Math.pow((x3 - x1), 2) + Math.pow((y3 - y1), 2));
+        if (a + b <= c || a + c <= b || a + c <= b) {
+            System.out.println("Треугольник не существует");
+        } else {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.x3 = x3;
+            this.y3 = y3;
+        }
     }
 
-    //ширина
+    //ширина треугольника
     public double getWidth() {
         if (x1 >= x2 && x2 >= x3) {
             return x1 - x3;
@@ -30,6 +35,7 @@ public class Triangle extends Shape {
         }
     }
 
+    //длина треугольника
     public double getHeight() {
         if (y1 >= y2 && y2 >= y3) {
             return y1 - y3;
@@ -48,26 +54,3 @@ public class Triangle extends Shape {
         return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
     }
 }
-/*
-    private static double getLength(double x1, double y1, double x2, double y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    }
-
-    private static double getSquare(double a, double b, double c) {
-        double halfPerimeter = (a + b + c) / 2;
-        return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
-    }
-
-        double a = getLength(x1, y1, x2, y2);
-        double b = getLength(x2, y2, x3, y3);
-        double c = getLength(x1, y1, x3, y3);
-
-        if (a + b <= c || a + c <= b || a + c <= b) {
-            System.out.println("Треугольник не существует");
-        } else {
-            double square = getSquare(a, b, c);
-            System.out.println("Площадь треугольника = " + square);
-        }
-
-
- */
