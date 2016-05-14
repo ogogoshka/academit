74bc17e29763ac1e49c485a046a1c1e420ec87ee
ogogoshka <xxxpiracy@gmail.com>
2016-05-14 23:00:45 +0600

    проект Vector

--- Vector/src/MainVector.java
+++ Vector/src/MainVector.java
@@ -9,7 +9,6 @@ import static ru.academit.novikov.vector.Vector.sum;
 public class MainVector {
     public static void main(String[] args) {
 
-
         double[] array = {1, 2, 3, 4};
 
         Vector v44 = new Vector(4, array);
@@ -17,21 +16,16 @@ public class MainVector {
 
         Vector v30 = new Vector(4);
         System.out.println(v30.toString());
-        //v30.add(v44);
-        //v30.minus(v44);
 
         Vector v1 = new Vector(4);
 
         sum(v30, v44);
-        sum(v30, v44).toString();
 
         System.out.println(Arrays.toString(sum(v30, v44)));
-        System.out.println(sum(v30, v44).toString());
 
         Vector v2 = new Vector(4, array);
         Vector v3 = new Vector(4);
         System.out.println(Arrays.toString(difference(v2, v3)));
-
 
         scalarMultiplication(v2, v3);
         System.out.println(scalarMultiplication(v2, v3));
