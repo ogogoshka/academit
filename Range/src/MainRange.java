f782e0773802386934b07c7e2fcf6a7d8e8f7c08
ogogoshka <xxxpiracy@gmail.com>
2016-05-16 11:27:17 +0600

    проект Range

--- Range/src/MainRange.java
+++ Range/src/MainRange.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class MainRange {
     public static void main(String[] args) {
-
         Range r1 = new Range(7, 17);
         Range r2 = new Range(1, 11);
         Range r3 = new Range();
-        Range[] r4;
-        Range[] r5;
+
+        r1.lengthRange();
+        r1.getFrom();
+        r1.getTo();
+
+        r3.setFrom(1);
+        r3.setTo(11);
 
         r1.intersection(r2);
         System.out.println(r1.intersection(r2).toString());
@@ -17,5 +21,7 @@ public class MainRange {
         r1.difference(r2);
         System.out.println(Arrays.toString(r1.difference(r2)));
 
+        r1.combination(r2);
+        System.out.println(Arrays.toString(r1.combination(r2)));
     }
 }
