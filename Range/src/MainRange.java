e5225bf4fa12e371fcf1059bc3c36b173d0a085c
ogogoshka <xxxpiracy@gmail.com>
2016-05-17 11:23:18 +0600

    проект Range

--- Range/src/MainRange.java
+++ Range/src/MainRange.java
@@ -1,9 +1,11 @@
 import ru.academit.novikov.range.Range;
 
+import java.util.Arrays;
+
 public class MainRange {
     public static void main(String[] args) {
-        Range r1 = new Range(22, 29);
-        Range r2 = new Range(20, 29);
+        Range r1 = new Range(11, 15);
+        Range r2 = new Range(11, 12);
         Range r3 = new Range();
 
         r1.lengthRange();
@@ -13,15 +15,13 @@ public class MainRange {
         r3.setFrom(1);
         r3.setTo(11);
 
-        //System.out.println(r1.isIntersection(r2));
-
         r1.intersection(r2);
         System.out.println(r1.intersection(r2).toString());
 
-        //r1.difference(r2);
-        //System.out.println(Arrays.toString(r1.difference(r2)));
+        r1.difference(r2);
+        System.out.println(Arrays.toString(r1.difference(r2)));
 
-        //r1.union(r2);
-        //System.out.println(Arrays.toString(r1.union(r2)));
+        r1.union(r2);
+        System.out.println(Arrays.toString(r1.union(r2)));
     }
 }
--- Range/src/ru/academit/novikov/range/Range.java
+++ Range/src/ru/academit/novikov/range/Range.java
@@ -49,106 +49,65 @@ public class Range {
         return isInside(r2.from) || isInside(r2.to);
     }
 
-    public Range intersection(Range r2) {
-        if (this.to <= r2.from || r2.to <= this.from) {
+    public Range intersection(Range range) {
+        if (this.to <= range.from || range.to <= this.from) {
             return null;
         } else {
-            if (this.from > r2.from && r2.to <= this.to) {
-                return new Range(this.from, Math.min(r2.to, this.to));
-            } else if (this.to <= r2.to && this.from < r2.from) {
-                return new Range(Math.max(r2.from, this.from), this.to);
-            } else if (r2.from < this.from && r2.to > this.to) {
+            if (this.from > range.from && range.to <= this.to) {
+                return new Range(this.from, Math.min(range.to, this.to));
+            } else if (this.to <= range.to && this.from < range.from) {
+                return new Range(Math.max(range.from, this.from), this.to);
+            } else if (range.from < this.from && range.to > this.to) {
                 return new Range(this.from, this.to);
             } else {
-                return new Range(r2.from, r2.to);
+                return new Range(range.from, range.to);
             }
         }
     }
 
-    /*
-
-        public Range intersection(Range r2) {
-        if (!isIntersection(r2)) {
-            return null;
+    public Range[] union(Range r2) {
+        if (r2.from > this.to) {
+            Range[] newRange2 = new Range[2];
+            newRange2[0] = this;
+            newRange2[1] = r2;
+            return newRange2;
+        } else if (this.from > r2.to) {
+            Range[] newRange2 = new Range[2];
+            newRange2[0] = r2;
+            newRange2[1] = this;
+            return newRange2;
         } else {
-            if (this.from < r2.to && r2.to < this.to) {
-                return new Range(this.from, Math.min(r2.to, this.to));
-            } else if (this.to < r2.to && this.to > r2.from) {
-                return new Range(Math.min(r2.from, this.from), this.to);
-            } else {
-                return null;
-            }
+            Range[] newRange = new Range[1];
+            Range r3 = new Range(Math.min(this.from, r2.from), Math.max(r2.to, this.to));
+            newRange[0] = r3;
+            return newRange;
         }
     }
-
-
-     */
-    /*
-
-        public Range intersection(Range r2) {
-        Range r3 = new Range();
-
-        if (this.to <= r2.from || r2.to <= this.from) {
-            //Range r3 = new Range();
-            return null;
-        } else if (this.from <= r2.from && r2.to <= this.to) {
-            return new Range(Math.max(this.from, r2.from), Math.min(r2.to, this.to));
-        } else if (isInside(r2.from) && !isInside(r2.to)) {
-            return new Range(r2.from, this.to);
-        } else if (!isInside(r2.from) && isInside(r2.to)) {
-            return new Range(this.from, r2.to);
-        }
-        return r3;
-    }
-
-     */
-
 
     public Range[] difference(Range r2) {
-        //Range r3 = new Range();
-        //Range r4 = new Range();
-        Range[] newRange = new Range[1];
-        if (this.to < r2.from || r2.to < this.from) {
-            newRange[0] = new Range(this.from, this.to);
-        } else if (this.from < r2.from && r2.to < this.to) {
-            Range[] newRange2 = new Range[2];
-            newRange2[0] = new Range(this.from, r2.from);
-            newRange2[1] = new Range(r2.to, this.to);
-            return newRange2;
-        } else if (this.from == r2.from && r2.to == this.to) {
-            newRange[0] = new Range(this.from, this.to);
-        } else if (isInside(r2.from) && !isInside(r2.to)) {
-            Range[] newRange2 = new Range[2];
-            newRange2[0] = new Range(this.from, r2.from);
-            newRange2[1] = new Range(this.to, r2.to);
-            return newRange2;
-        } else if (!isInside(r2.from) && isInside(r2.to)) {
+        if (this.from > r2.from && r2.to < this.to) {
             Range[] newRange2 = new Range[2];
             newRange2[0] = new Range(r2.from, this.from);
             newRange2[1] = new Range(r2.to, this.to);
             return newRange2;
-        }
-        return newRange;
-    }
-
-    //объединение
-    public Range[] union(Range r2) {
-        Range[] newRange = new Range[1];
-        if (isInside(r2.from) || isInside(r2.to)) {
-            Range r3 = new Range(Math.min(this.from, r2.from), Math.max(r2.to, this.to));
-            newRange[0] = r3;
-        } else if (this.to < r2.from || r2.to < this.from) {
+        } else if (this.to < r2.to && this.from < r2.from) {
             Range[] newRange2 = new Range[2];
-            if (this.to < r2.to) {
-                newRange2[0] = this;
-                newRange2[1] = r2;
-                return newRange2;
-            } else {
-                newRange2[0] = r2;
-                newRange2[1] = this;
-                return newRange2;
-            }
+            newRange2[0] = new Range(this.from, r2.from);
+            newRange2[1] = new Range(this.to, r2.to);
+            return newRange2;
+        } else if (r2.from > this.from && r2.to < this.to) {
+            Range[] newRange2 = new Range[2];
+            newRange2[0] = new Range(this.from, r2.from);
+            newRange2[1] = new Range(r2.to, this.to);
+            return newRange2;
+        } else if (this.from > r2.from && this.to < r2.to) {
+            Range[] newRange2 = new Range[2];
+            newRange2[0] = new Range(r2.from, this.from);
+            newRange2[1] = new Range(this.to, r2.to);
+            return newRange2;
+        } else {
+            Range[] newRange = new Range[1];
+            return newRange;
         }
-        return newRange;
     }
 }
