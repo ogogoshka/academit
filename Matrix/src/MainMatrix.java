4915bd29e2bc7ee4b2a23deafb9632f46fe88bcc
ogogoshka <xxxpiracy@gmail.com>
2016-05-19 17:44:13 +0600

    проект MATRIX

--- Matrix/src/MainMatrix.java
+++ Matrix/src/MainMatrix.java
@@ -1,7 +1,12 @@
 import ru.academit.novikov.matrix.Matrix;
 
+import java.util.Vector;
+
 public class MainMatrix {
     public static void main(String[] args) {
+
+        double[] array = {1, 2, 5, 6};
+        Vector v5 = new Vector(3, array);
 
 
         Matrix m1 = new Matrix(3, 3);
@@ -11,19 +16,19 @@ public class MainMatrix {
         int[][] ages = new int[10][5];
         double[][] arr1 = {{1, 2}, {5, 6}, {8, 9}};
 
+
         Matrix m2 = new Matrix(arr1);
         System.out.println(m2.toString());
 
-        double[][] temp = new double[arr1.length][arr1.length];
-        //System.out.println(Arrays.toString(temp));
+        //double[][] temp = new double[arr1.length][arr1.length];
 
-        System.arraycopy(arr1, 0, temp, 0, arr1.length);
+        //System.arraycopy(arr1, 0, temp, 0, arr1.length);
 
-        System.out.println(arr1.length + " x " + arr1[0].length);
+        //System.out.println(arr1.length + " x " + arr1[0].length);
 
         Matrix m3 = new Matrix(m2);
 
-        System.out.println(m3.toString());
+        //System.out.println(m3.toString());
 
         //System.out.println(Arrays.toString(arr1));
         //System.out.println(Arrays.toString(temp));
@@ -31,6 +36,25 @@ public class MainMatrix {
         //System.out.println("ages.length = " + ages.length);
         //System.out.println("ages[0].length = " + ages[0].length);
 
+        //int[][] temp2;
+        //temp2 = m1.getSize();
+
         //System.out.println(Arrays.toString(m1.getSize()));
+        //System.out.println(Arrays.toString(temp2));
+        //m1.getSize2();
+        System.out.println(m2.getSize2().toString());
+
+        //m2.getVectorLine(1);
+        //System.out.println(m2.getVectorLine(1).toString());
+
+        //m2.getTranspose();
+        //System.out.println(m2.getTranspose().toString());
+
+        //m2.multScalar(2);
+        System.out.println(m2.multScalar(3).toString());
+
+        System.out.println(m2.getVectorLine2(1).toString());
+
+
     }
 }
--- Matrix/src/ru/academit/novikov/matrix/Matrix.java
+++ Matrix/src/ru/academit/novikov/matrix/Matrix.java
@@ -1,40 +1,111 @@
 package ru.academit.novikov.matrix;
 
+import java.util.Vector;
+
 public class Matrix {
 
     private int sizeN;
     private int sizeM;
-    private double[][] components;
+    private double[][] elements;
+
+    public Matrix() {
+
+    }
 
     public Matrix(int sizeN, int sizeM) {
         if (sizeN < 0 || sizeM < 0) {
             throw new IllegalArgumentException("размеры матрицы не могут быть отрицательными");
         }
-        components = new double[sizeN][sizeM];
+        elements = new double[sizeN][sizeM];
     }
 
+    public Matrix getVectorLine(int numberLine) {
+        //double[] temp = new double[this.elements[0].length];
+        Matrix m2 = new Matrix(1, this.elements[0].length);
+        System.arraycopy(this.elements[numberLine], 0, m2.elements[0], 0, this.elements[0].length);
+        return m2;
+    }
+
+    public Vector getVectorLine2(int numberLine) {
+
+        Vector[] newVector = new Vector[1];
+        newVector[0] = new Vector(this.elements[0].length);
+
+        Vector v2 = new Vector(this.elements[0].length);
+
+
+        double[] temp = new double[this.elements[0].length];
+        Matrix m2 = new Matrix(1, this.elements[0].length);
+        //System.arraycopy(this.elements[numberLine], 0, v2.components, 0, this.elements[0].length);
+
+        for (int i = 0; i < this.elements[0].length; i++) {
+            temp[i] = this.elements[numberLine][i];
+        }
+
+        Vector v3 = new Vector(this.elements[0].length);
+
+        return v3;
+    }
+
+
     public Matrix(Matrix m2) {
-        double[][] temp = new double[m2.components.length][];
-        System.arraycopy(m2.components, 0, temp, 0, m2.components.length);
-        this.components = temp;
+        double[][] temp = new double[m2.elements.length][];
+        System.arraycopy(m2.elements, 0, temp, 0, m2.elements.length);
+        this.elements = temp;
 
     }
 
     public Matrix(double[][] array) {
         double[][] temp = new double[array.length][];
         System.arraycopy(array, 0, temp, 0, array.length);
-        this.components = temp;
+        this.elements = temp;
     }
 
     public int[][] getSize() {
         this.sizeN = sizeN;
         this.sizeM = sizeM;
-        //components.length = sizeN;
-        //components[0].length = sizeM;
+        //elements.length = sizeN;
+        //elements[0].length = sizeM;
         int[][] temp = new int[1][2];
-        temp[0][0] = sizeN;
-        temp[0][1] = sizeM;
+        temp[0][0] = this.elements.length;
+        temp[0][1] = this.elements[0].length;
         return temp;
+    }
+
+    public Matrix getTranspose() {
+        Matrix m2 = new Matrix(this.elements[0].length, this.elements.length);
+        for (int i = 0; i < this.elements.length; i++) {
+            for (int j = 0; j < this.elements[0].length; j++) {
+                double temp = this.elements[i][j];
+                m2.elements[j][i] = temp;
+            }
+        }
+        return m2;
+    }
+
+    public Matrix multScalar(double scalar) {
+        //Matrix m2 = new Matrix(this.elements[0].length, this.elements.length);
+        for (int i = 0; i < this.elements.length; i++) {
+            for (int j = 0; j < this.elements[0].length; j++) {
+                this.elements[i][j] = this.elements[i][j] * scalar;
+                //m2.elements[i][j] = temp;
+            }
+        }
+        return this;
+    }
+
+    public Matrix getSize2() {
+        this.sizeN = sizeN;
+        this.sizeM = sizeM;
+        //elements.length = sizeN;
+        //elements[0].length = sizeM;
+        Matrix m2 = new Matrix(1, 2);
+        //int[][] temp = new int[1][2];
+        //temp[0][0] = this.elements.length;
+        //temp[0][1] = this.elements[0].length;
+        m2.elements[0][0] = this.elements.length;
+        m2.elements[0][1] = this.elements[0].length;
+        return m2;
     }
 
 
@@ -43,16 +114,16 @@ public class Matrix {
         StringBuilder resultString = new StringBuilder();
         resultString.append("{ ");
 
-        for (int i = 0; i < components.length; i++) {
-            for (int j = 0; j < components[i].length; j++) {
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = 0; j < elements[i].length; j++) {
                 if (j == 0) {
                     resultString.append("{ ");
-                    resultString.append(this.components[i][j]);
+                    resultString.append(this.elements[i][j]);
                 } else {
-                    resultString.append(", ").append(this.components[i][j]);
+                    resultString.append(", ").append(this.elements[i][j]);
                 }
             }
-            if (i == components.length - 1) {
+            if (i == elements.length - 1) {
                 resultString.append(" }");
             } else {
                 resultString.append(" } , ");
