81ff84efbf1a487bd90d887adf469dd95a82979e
ogogoshka <xxxpiracy@gmail.com>
2016-05-20 11:00:34 +0600

    проект MATRIX

--- Matrix/src/MainMatrix.java
+++ Matrix/src/MainMatrix.java
@@ -1,7 +1,5 @@
 import ru.academit.novikov.matrix.Matrix;
-import ru.academit.novikov.vector.*;
-
-import static ru.academit.novikov.matrix.Matrix.multiplication;
+import ru.academit.novikov.vector.Vector;
 
 public class MainMatrix {
     public static void main(String[] args) {
@@ -11,21 +9,11 @@ public class MainMatrix {
         double[][] array3 = {{1, 2, 3}, {4, 5, 6}};
 
         Matrix ar2 = new Matrix(array3);
-        //System.out.println(ar2.toString());
 
-        //Matrix ar3 = new Matrix(array3);
-        //System.out.println(ar2.toString());
-
-        //System.arraycopy(array2[0], 0, array3[0], 0, array2[0].length);
         Vector v5 = new Vector(4, array);
         ar2.MultiplicationByVector(v5);
-        //Matrix m1 = new Matrix(3, 3);
-        //System.out.println(m1.toString());
-        //int[][] ages = new int[10][5];
-        //double[][] arr1 = {{1, 2}, {5, 6}, {8, 9}};
 
         Matrix m2 = new Matrix(array3);
-        //System.out.println(m2.toString());
 
         System.out.println(m2.getTranspose().toString());
 
@@ -34,28 +22,16 @@ public class MainMatrix {
         m2.getSize();
 
         Matrix m3 = new Matrix(ar2);
-        //System.out.println(m3.toString());
 
         m3.getVectorLine(0);
         m3.getVectorColumn(0);
-        //System.out.println(m3.getVectorLine(0).toString());
-
-        Matrix m11 = new Matrix();
-        Matrix m22 = new Matrix(3, 3);
-        //System.out.println(m22.toString());
 
         Vector v22 = new Vector(2, array);
-        m22.setVectorLine(1, v22);
-
-        //System.out.println(m22.toString());
 
         Matrix m30 = new Matrix(array3);
         Matrix m31 = new Matrix(array2);
-        //System.out.println("m30 = " + m30.toString());
-        //System.out.println("m31 = " + m31.toString());
 
         m30.add(m31);
-        //System.out.println(m30.toString());
 
         double[][] array40 = {{1, 2}, {3, 4}, {5, 6}};
         double[][] array41 = {{1, 2}, {4, 5}};
@@ -63,63 +39,10 @@ public class MainMatrix {
         Matrix m40 = new Matrix(array41);
         Matrix m41 = new Matrix(array40);
 
-        //System.out.println("m40 = " + m40.toString());
-        //System.out.println("m41 = " + m41.toString());
-        m40.minus(m41);
-        //System.out.println("MIN = " + m40.toString());
-
-        //m2.MultiplicationByVector(v5);
-
-        //System.out.println(m2.MultiplicationByVector(v5).toString());
-
-        //System.out.println(m2.toString());
-
-        //double[][] temp = new double[arr1.length][arr1.length];
-
-        //System.arraycopy(arr1, 0, temp, 0, arr1.length);
-
-        //System.out.println(arr1.length + " x " + arr1[0].length);
-
-        //Matrix m3 = new Matrix(m2);
-
-        //System.out.println(m3.toString());
-
-        //System.out.println(Arrays.toString(arr1));
-        //System.out.println(Arrays.toString(temp));
-
-        //System.out.println("ages.length = " + ages.length);
-        //System.out.println("ages[0].length = " + ages[0].length);
-
-        //int[][] temp2;
-        //temp2 = m1.getSize();
-
-        //System.out.println(Arrays.toString(m1.getSize()));
-        //System.out.println(Arrays.toString(temp2));
-        //m1.getSize2();
-        //System.out.println(m2.getSize2().toString());
-
-        //m2.getVectorLine(1);
-        //System.out.println(m2.getVectorLine(1).toString());
-
-        m2.getTranspose();
-        //System.out.println(m2.getTranspose().toString());
-        //m2.multScalar(2);
-        //System.out.println(m2.toString());
-
-        //m2.getSize();
-        //System.out.println(Arrays.toString(m2.getSize()));
-
         double[][] array50 = {{1}, {2}, {3}};
         double[][] array51 = {{1, 2}, {4, 5}};
 
         double[] array55 = {1, 2, -1};
-
-        Vector v50 = new Vector(3, array55);
-
-        Matrix m50 = new Matrix(array50);
-        Matrix m51 = new Matrix(array51);
-
-        //System.out.println(m50.MultiplicationByVector(v50));
 
         double[][] arr1 = {{2, 4, 0}, {-2, 1, 3}, {-1, 0, 1}};
         double[] arr2 = {1, 2, -1};
@@ -127,17 +50,6 @@ public class MainMatrix {
         double[][] arr11 = {{-1, 1}, {2, 0}, {0, 3}};
         double[][] arr12 = {{3, 1, 2}, {0, -1, 4}};
 
-        Matrix mm50 = new Matrix(arr1);
-        Vector vv50 = new Vector(3, arr2);
-
-        Matrix mmm1 = new Matrix(arr11);
-        Matrix mmm2 = new Matrix(arr12);
-
-        //System.out.println(mm50.MultiplicationByVector(vv50));
-
-        //mm50.getVectorColumn(1);
-        multiplication(mmm1, mmm2);
-        //System.out.println(multiplication(mmm1, mmm2).toString());
 
     }
 }
--- Matrix/src/ru/academit/novikov/matrix/Matrix.java
+++ Matrix/src/ru/academit/novikov/matrix/Matrix.java
@@ -9,16 +9,16 @@ public class Matrix {
     public Matrix() {
     }
 
-    public Matrix(int sizeN, int sizeM) {
-        if (sizeN < 0 || sizeM < 0) {
+    public Matrix(int numbersLine, int numbersColumn) {
+        if (numbersLine < 0 || numbersColumn < 0) {
             throw new IllegalArgumentException("размеры матрицы не могут быть отрицательными");
         }
-        elements = new double[sizeN][sizeM];
+        elements = new double[numbersLine][numbersColumn];
     }
 
-    public Matrix(Matrix m2) {
-        double[][] temp = new double[m2.elements.length][m2.elements[0].length];
-        System.arraycopy(m2.elements, 0, temp, 0, m2.elements.length);
+    public Matrix(Matrix matrix) {
+        double[][] temp = new double[matrix.elements.length][matrix.elements[0].length];
+        System.arraycopy(matrix.elements, 0, temp, 0, matrix.elements.length);
         this.elements = temp;
     }
 
@@ -28,29 +28,21 @@ public class Matrix {
         this.elements = temp;
     }
 
-    //d.	Matrix(Vector[]) – из массива векторов-строк
-
-    public Matrix(Vector[] v2) {
-
-        int lengthV = v2.length;
-
-        //Matrix m3 = v2[v2.length];
-
-
-        Vector[] v3 = new Vector[5];
-
-        for (int i = 0; i < v2.length; i++) {
-            //for (int j = 0; j < v2.length[getSize()]; j++) {
-            //this.elements[i][j] = v2.;
+    //d. Matrix(Vector[]) – из массива векторов-строк
+    public Matrix(Vector[] vector) {
+        this.elements = new double[vector.length][vector[0].getSize()];
+        for (int i = 0; i < vector.length; i++) {
+            for (int j = 0; j < vector[0].getSize(); j++) {
+                this.elements[i][j] = vector[i].getComponent(j);
+            }
         }
     }
 
-
     public Matrix getSize() {
-        Matrix m2 = new Matrix(1, 2);
-        m2.elements[0][0] = this.elements.length;
-        m2.elements[0][1] = this.elements[0].length;
-        return m2;
+        Matrix matrix = new Matrix(1, 2);
+        matrix.elements[0][0] = this.elements.length;
+        matrix.elements[0][1] = this.elements[0].length;
+        return matrix;
     }
 
     public Vector getVectorLine(int numberLine) {
@@ -64,11 +56,11 @@ public class Matrix {
         return new Vector(this.elements[0].length, temp);
     }
 
-    public void setVectorLine(int numberLine, Vector v2) {
-        int minLength = Math.min(v2.getSize(), this.elements[0].length);
+    public void setVectorLine(int numberLine, Vector vector) {
+        int minLength = Math.min(vector.getSize(), this.elements[0].length);
         double[] temp = new double[this.elements[0].length];
         for (int i = 0; i < minLength; i++) {
-            temp[i] = v2.getComponent(i);
+            temp[i] = vector.getComponent(i);
             this.elements[numberLine][i] = temp[i];
         }
     }
@@ -85,15 +77,15 @@ public class Matrix {
     }
 
     public Matrix getTranspose() {
-        Matrix m2 = new Matrix(this.elements[0].length, this.elements.length);
+        Matrix matrix = new Matrix(this.elements[0].length, this.elements.length);
         for (int i = 0; i < this.elements.length; i++) {
             for (int j = 0; j < this.elements[0].length; j++) {
                 double temp = this.elements[i][j];
-                m2.elements[j][i] = temp;
+                matrix.elements[j][i] = temp;
             }
         }
         this.elements = new double[this.elements[0].length][this.elements.length];
-        this.elements = m2.elements;
+        this.elements = matrix.elements;
         return this;
     }
 
@@ -105,7 +97,7 @@ public class Matrix {
         }
     }
 
-//f.	Вычисление определителя матрицы
+//f. Вычисление определителя матрицы
 
     @Override
     public String toString() {
@@ -129,88 +121,44 @@ public class Matrix {
         return resultString.append(" }").toString();
     }
 
-    public Matrix MultiplicationByVector(Vector v2) {
-        //if (this.elements[0].length != v2.getSize()) {
-        //throw new IllegalArgumentException("нельзя умножать");
-        //} else {
+    public Matrix add(Matrix matrix) {
+        int maxNumbersLine = Math.max(matrix.elements.length, this.elements.length);
+        int maxNumbersColumn = Math.max(matrix.elements[0].length, this.elements[0].length);
 
-        if (this.elements.length == v2.getSize() && this.elements[0].length == 1) {
-
-            double[][] temp = new double[this.elements.length][this.elements.length];
-
-            for (int i = 0; i < this.elements.length; i++) {
-                for (int j = 0; j < this.elements.length; j++) {
-                    temp[i][j] = this.elements[i][0] * v2.getComponent(j);
-                    //temp[i][0] = temp[i][0] + temp[i][0];
-                    //temp2 = temp2 + this.elements[i][j] * v2.components[j];
-                }
-            }
-            this.elements = temp;
-
-
-        } else if (this.elements[0].length > 1 && this.elements[0].length == v2.getSize()) {
-            double[][] temp = new double[this.elements.length][this.elements[0].length];
-            //double temp2 = 0;
-            for (int i = 0; i < this.elements.length; i++) {
-                temp[i][0] = 0;
-                for (int j = 0; j < this.elements[0].length; j++) {
-                    temp[i][0] = temp[i][0] + this.elements[i][j] * v2.getComponent(j);
-                    //temp[i][0] = temp[i][0] + temp[i][0];
-                    //temp2 = temp2 + this.elements[i][j] * v2.components[j];
-                }
-            }
-            this.elements = temp;
-        } else {
-            throw new IllegalArgumentException("нельзя умножать");
-        }
-        //}
-        //this.elements = temp;
-        return this;
-    }
-
-    public Matrix add(Matrix m2) {
-        int maxLengthI = Math.max(m2.elements.length, this.elements.length);
-        int maxLengthJ = Math.max(m2.elements[0].length, this.elements[0].length);
-
-        double[][] temp = new double[maxLengthI][maxLengthJ];
+        double[][] temp = new double[maxNumbersLine][maxNumbersColumn];
         for (int i = 0; i < this.elements.length; i++) {
             System.arraycopy(this.elements[i], 0, temp[i], 0, this.elements[0].length);
         }
 
-        for (int i = 0; i < m2.elements.length; i++) {
-            for (int j = 0; j < m2.elements[0].length; j++) {
-                temp[i][j] = temp[i][j] + m2.elements[i][j];
+        for (int i = 0; i < matrix.elements.length; i++) {
+            for (int j = 0; j < matrix.elements[0].length; j++) {
+                temp[i][j] = temp[i][j] + matrix.elements[i][j];
             }
         }
-        this.elements = new double[maxLengthI][maxLengthJ];
+        this.elements = new double[maxNumbersLine][maxNumbersColumn];
         this.elements = temp;
         return this;
     }
 
-    public Matrix minus(Matrix m2) {
-        int maxLengthI = Math.max(m2.elements.length, this.elements.length);
-        int maxLengthJ = Math.max(m2.elements[0].length, this.elements[0].length);
+    public Matrix minus(Matrix matrix) {
+        int maxNumbersLine = Math.max(matrix.elements.length, this.elements.length);
+        int maxNumbersColumn = Math.max(matrix.elements[0].length, this.elements[0].length);
 
-        double[][] temp = new double[maxLengthI][maxLengthJ];
+        double[][] temp = new double[maxNumbersLine][maxNumbersColumn];
         for (int i = 0; i < this.elements.length; i++) {
-            /*
-                for (int j = 0; j < this.elements[0].length; j++) {
-                temp[i][j] = this.elements[i][j];
-            } можно заменить на arraycopy
-             */
+
             System.arraycopy(this.elements[i], 0, temp[i], 0, this.elements[0].length);
         }
 
-        for (int i = 0; i < m2.elements.length; i++) {
-            for (int j = 0; j < m2.elements[0].length; j++) {
-                temp[i][j] = temp[i][j] - m2.elements[i][j];
+        for (int i = 0; i < matrix.elements.length; i++) {
+            for (int j = 0; j < matrix.elements[0].length; j++) {
+                temp[i][j] = temp[i][j] - matrix.elements[i][j];
             }
         }
-        this.elements = new double[maxLengthI][maxLengthJ];
+        this.elements = new double[maxNumbersLine][maxNumbersColumn];
         this.elements = temp;
         return this;
     }
-
 
     public static Matrix add(Matrix m1, Matrix m2) {
         Matrix m3 = new Matrix(m1);
@@ -221,101 +169,4 @@ public class Matrix {
         Matrix m3 = new Matrix(m1);
         return m3.minus(m2);
     }
-
-    public static Matrix multiplication(Matrix m1, Matrix m2) {
-
-        //if (this.elements[0].length != v2.getSize()) {
-        //throw new IllegalArgumentException("нельзя умножать");
-        //} else {
-
-        int maxI = Math.max(m1.elements.length, m2.elements.length);
-        int maxJ = Math.max(m1.elements[0].length, m2.elements[0].length);
-        double[][] temp = new double[m1.elements.length][m1.elements.length];
-
-        //temp[i][0] = 0;
-        //for (int j = 0; j < this.elements[0].length; j++) {
-        //temp[i][0] = temp[i][0] + this.elements[i][j] * v2.getComponent(j);
-        //temp[i][0] = temp[i][0] + temp[i][0];
-        //temp2 = temp2 + this.elements[i][j] * v2.components[j];
-
-        if (m1.elements[0].length == m2.elements.length) {
-            for (int i = 0; i < m1.elements.length; i++) {
-                for (int j = 0; j < m1.elements.length; j++) {
-                    temp[i][j] = m1.elements[i][j] * m2.elements[j][i];
-                    //temp[i][j] = temp[i][j] + temp[i][j];
-                }
-
-            }
-
-            double[][] temp2 = new double[m1.elements[0].length][m1.elements[0].length];
-
-            for (int i = 0; i < m1.elements[0].length; i++) {
-                for (int j = 0; j < m1.elements[0].length; j++) {
-                    temp2[i][j] = m1.elements[i][j] * m2.elements[j][i];
-                    //temp[i][j] = temp[i][j] + temp[i][j];
-                }
-
-            }
-
-        } else {
-            throw new IllegalArgumentException("нельзя умножать");
-        }
-        Matrix m3 = new Matrix(temp);
-        return m3;
-    }
-
-/*
-        if (this.elements.length == v2.getSize() && this.elements[0].length == 1) {
-
-            double[][] temp = new double[this.elements.length][this.elements.length];
-
-            for (int i = 0; i < this.elements.length; i++) {
-                for (int j = 0; j < this.elements.length; j++) {
-                    temp[i][j] = this.elements[i][0] * v2.getComponent(j);
-                    //temp[i][0] = temp[i][0] + temp[i][0];
-                    //temp2 = temp2 + this.elements[i][j] * v2.components[j];
-                }
-            }
-            this.elements = temp;
-
-
-        } else if (this.elements[0].length > 1 && this.elements[0].length == v2.getSize()) {
-            double[][] temp = new double[this.elements.length][this.elements[0].length];
-            //double temp2 = 0;
-            for (int i = 0; i < this.elements.length; i++) {
-                temp[i][0] = 0;
-                for (int j = 0; j < this.elements[0].length; j++) {
-                    temp[i][0] = temp[i][0] + this.elements[i][j] * v2.getComponent(j);
-                    //temp[i][0] = temp[i][0] + temp[i][0];
-                    //temp2 = temp2 + this.elements[i][j] * v2.components[j];
-                }
-            }
-            this.elements = temp;
-        } else {
-            throw new IllegalArgumentException("нельзя умножать");
-        }
-
-        //}
-
-
-        //this.elements = temp;
-        return m2;
-    }
- */
-
-
-    //return m2;
-    //}
-
-
 }
-
-
-/*
-    public int[][] getSize2() {
-        int[][] temp = new int[1][2];
-        temp[0][0] = this.elements.length;
-        temp[0][1] = this.elements[0].length;
-        return temp;
-    }
- */
