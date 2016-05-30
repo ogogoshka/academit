256ad616a6e5a2cd4417649c820d3e3cf4f882f9
ogogoshka <xxxpiracy@gmail.com>
2016-05-30 11:22:42 +0600

    GaussMethod

--- ConvertTemperatureSwing/src/ru/academit/novikov/flowlayout/TemperatureGUI.java
+++ ConvertTemperatureSwing/src/ru/academit/novikov/flowlayout/TemperatureGUI.java
@@ -8,6 +8,15 @@ import java.awt.event.ActionListener;
 import static ru.academit.novikov.flowlayout.TemperatureConvert.*;
 
 public class TemperatureGUI {
+
+    private static final String FAHRENHEIT_IN = "Фаренгейтах";
+    private static final String KELVIN_IN = "Кельвинах";
+    private static final String CELSIUS_IN = "Цельсиях";
+
+    private static final String FAHRENHEIT_OUT = "Фаренгейты";
+    private static final String KELVIN_OUT = "Кельвины";
+    private static final String CELSIUS_OUT = "Цельсии";
+
     private JFrame jFrame = new JFrame("Тулза для перевода температур");
     private JTextField entryField = new JTextField(30);
     private JTextField outField = new JTextField(30);
@@ -28,9 +37,9 @@ public class TemperatureGUI {
 
         jPanel.add(labelTemperatureIn);
         jPanel.add(choiceTemperatureIn);
-        choiceTemperatureIn.add("Фаренгейтах");
-        choiceTemperatureIn.add("Цельсиях");
-        choiceTemperatureIn.add("Кельвинах");
+        choiceTemperatureIn.add(FAHRENHEIT_IN);
+        choiceTemperatureIn.add(CELSIUS_IN);
+        choiceTemperatureIn.add(KELVIN_IN);
 
         jPanel.add(entryField);
 
@@ -40,9 +49,9 @@ public class TemperatureGUI {
 
         jPanel.add(labelTemperatureOut);
         jPanel.add(choiceTemperatureOut);
-        choiceTemperatureOut.add("Фаренгейты");
-        choiceTemperatureOut.add("Цельсии");
-        choiceTemperatureOut.add("Кельвины");
+        choiceTemperatureOut.add(FAHRENHEIT_OUT);
+        choiceTemperatureOut.add(CELSIUS_OUT);
+        choiceTemperatureOut.add(KELVIN_OUT);
 
         convertButton.addActionListener(new ConvertActionListener());
     }
@@ -63,21 +72,21 @@ public class TemperatureGUI {
             throw new IllegalArgumentException("Температура должна быть числом!!!");
         }
 
-        if (choiceTemperatureIn.getSelectedItem().equals("Фаренгейтах")) {
+        if (choiceTemperatureIn.getSelectedItem().equals(FAHRENHEIT_IN)) {
             Double inF = Double.valueOf(entryString);
-            if (choiceTemperatureOut.getSelectedItem().equals("Фаренгейты")) {
+            if (choiceTemperatureOut.getSelectedItem().equals(FAHRENHEIT_OUT)) {
                 outField.setText(String.valueOf(inF));
-            } else if (choiceTemperatureOut.getSelectedItem().equals("Цельсии")) {
+            } else if (choiceTemperatureOut.getSelectedItem().equals(CELSIUS_OUT)) {
                 outField.setText(String.valueOf(fahrenheitToCelsius(inF)));
             } else {
                 outField.setText(String.valueOf(fahrenheitToKelvin(inF)));
             }
 
-        } else if (choiceTemperatureIn.getSelectedItem().equals("Цельсиях")) {
+        } else if (choiceTemperatureIn.getSelectedItem().equals(CELSIUS_IN)) {
             Double inC = Double.valueOf(entryString);
-            if (choiceTemperatureOut.getSelectedItem().equals("Фаренгейты")) {
+            if (choiceTemperatureOut.getSelectedItem().equals(FAHRENHEIT_OUT)) {
                 outField.setText(String.valueOf(celsiusToFahrenheit(inC)));
-            } else if (choiceTemperatureOut.getSelectedItem().equals("Цельсии")) {
+            } else if (choiceTemperatureOut.getSelectedItem().equals(CELSIUS_OUT)) {
                 outField.setText(String.valueOf(inC));
             } else {
                 outField.setText(String.valueOf(celsiusToKelvin(inC)));
@@ -85,9 +94,9 @@ public class TemperatureGUI {
 
         } else {
             Double inK = Double.valueOf(entryString);
-            if (choiceTemperatureOut.getSelectedItem().equals("Фаренгейты")) {
+            if (choiceTemperatureOut.getSelectedItem().equals(FAHRENHEIT_OUT)) {
                 outField.setText(String.valueOf(kelvinToFahrenheit(inK)));
-            } else if (choiceTemperatureOut.getSelectedItem().equals("Цельсии")) {
+            } else if (choiceTemperatureOut.getSelectedItem().equals(CELSIUS_OUT)) {
                 outField.setText(String.valueOf(kelvinToCelsius(inK)));
             } else {
                 outField.setText(String.valueOf(inK));
