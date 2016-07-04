15d91fb1dfd1b4f7b632ac6faf7aa862e5091a42
ogogoshka <xxxpiracy@gmail.com>
2016-07-04 19:24:46 +0600

    DONE

---
+++ GaussMethod/src/ru/academit/novikov/gauss/Result.java
@@ -0,0 +1,15 @@
+package ru.academit.novikov.gauss;
+
+import ru.academit.novikov.vector.Vector;
+
+public class Result {
+
+    private Vector vectorSolution;
+    private Solution numberOfSolution;
+
+    public Result() {
+
+    }
+
+
+}
