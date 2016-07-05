6ed87d72180d6325722d5c5a7582bc1a8a528b05
ogogoshka <xxxpiracy@gmail.com>
2016-07-05 12:55:29 +0600

    DONE

--- GaussMethod/src/ru/academit/novikov/gauss/Result.java
+++ GaussMethod/src/ru/academit/novikov/gauss/Result.java
@@ -7,9 +7,21 @@ public class Result {
     private Vector vectorSolution;
     private Solution numberOfSolution;
 
-    public Result(Solution numberOfSolution) {
+    public Result(Solution numberOfSolution, Vector vectorSolution) {
+        this.vectorSolution = vectorSolution;
         this.numberOfSolution = numberOfSolution;
     }
 
 
+    public Solution getNumberOfSolution() {
+        return numberOfSolution;
+    }
+
+    public Vector getVectorSolution() {
+        return vectorSolution;
+    }
+
+    public static void printSolution(Vector vector) {
+        System.out.println(vector.toString());
+    }
 }
