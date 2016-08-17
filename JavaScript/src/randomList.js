22eb613c9aae866b0313bcec20c21af9335cb695
ogogoshka <xxxpiracy@gmail.com>
2016-08-17 12:03:48 +0600

    DONE

--- JavaScript/src/randomList.js
+++ JavaScript/src/randomList.js
@@ -1,16 +1,15 @@
 var list = [23, 1, 11, 90, 33, 4, 59, 15, 66, 33, 5, 76, 22, 1];
 
-function compareNumbers(e1, e2) {
-    return e2 - e1;
-}
+list.sort(function (a, b) {
+    return b - a;
+});
 
-list.sort(compareNumbers);
-console.log(list.join(", "));
+console.log("sortedList = " + list.join(", "));
 
 var list2 = list.slice(0, 5);
 console.log("list2 = " + list2.join(", "));
 
-var list5 = list.reverse().slice(0, 5);
+var list5 = list.reverse().slice(0, 5).reverse();
 console.log("list5 = " + list5.join(", "));
 
 var totalList = list2.concat(list5);
@@ -24,4 +23,3 @@ for (var i = 0; i < totalList.length; ++i) {
 }
 
 console.log("sumEvenNumbers = " + sumEvenNumbers);
-
