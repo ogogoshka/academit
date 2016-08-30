7d0cf05a1f3e65aef7ae334a43a81a93e85e2fe4
ogogoshka <xxxpiracy@gmail.com>
2016-08-31 01:14:15 +0600

    DONE

--- PhoneBook/src/js/phonebook.js
+++ PhoneBook/src/js/phonebook.js
@@ -1,25 +1,112 @@
 var userNumber = 1;
 
-var reordersRow = function () {
-    var rows = $(".mainTable").find("tbody").find("tr");
-    userNumber = rows.length + 1;
-    $(rows).each(function (i) {
-        $(this).find("td").eq(1).text(i + 1);
-    })
-};
-
-function confirmDelete(row) {
-    var res = confirm("Вы действительно хотите удалить этого пользователя?");
-    if (res) {
-        row.closest('tr').remove();
-        reordersRow();
-    }
-}
 
 $(document).ready(function () {
-    var surname = "";
-    var name = "";
-    var phoneNumber = "";
+
+    var reordersRow = function () {
+        var rows = $(".mainTable").find("tbody").find("tr");
+        userNumber = rows.length + 1;
+        $(rows).each(function (i) {
+            $(this).find("td").eq(1).text(i + 1);
+        })
+    };
+
+
+    $(".ButtonForDeleteUser").on('click', function () {
+        var checked = $("input:checkbox:checked").map(function () {
+            //var checked = $("#check").is(":checked").map(function () {
+            return this.value;
+        }).get();
+
+
+        //var deleteList = $("#check:checked").closest("tr").find("td").eq(2).get();
+        //console.log("deleteList = " + deleteList);
+
+        if (confirm("Вы действительно хотите удалить эти контакты?")) {
+            $("#check:checked").parents("tr").remove();
+            reordersRow();
+        }
+
+        //$("#check").is(":checked").parents("tr").remove();
+        //$("input:checkbox:checked").parents("tr").remove();
+
+
+        //$("#all").attr("checked","checked");
+        //$(".mainTable tr:first-child").find("input#all").removeAttr("checked");
+
+    });
+
+    function confirmDelete(deleteCell) {
+        //row.closest("tr").find("td").eq(3);
+        //console.log(row.closest("tr").find("td").eq(3));
+        //var familia  = row.closest("tr").find("td")[3];
+        //var imya  = row.closest("tr").find("td")[4];
+
+        //var ff = row.closest("tr").find("td")[2];
+        //console.log($("td").text(ff));
+        //$(row).(function () {
+
+
+        //var isMarried = $("#check").is(":checked");
+        //console.log(isMarried);
+
+
+        //$(".mainTable tr").find("#check").each(function () {
+        //if ($(this).attr("checked")) {
+        //$(this).attr("checked", true);
+        //console.log("allilyia");
+
+        //}
+        //});
+        $(".mainTable tr").each(function () {
+            //var ff = $(this).find("td").eq(0).html();
+            //var ff = $(this).find("td").eq(0);
+            //var isMarried = $("#check").is(":checked");
+            var isMarried = $(this).find("#check").is(":checked");
+            //ff.find("span");
+            //console.log(ff);
+            //console.log(ff.find("span"));
+            if (isMarried) {
+                console.log(isMarried);
+            }
+        });
+
+
+        /*
+         $(".mainTable tr").each(function () {
+         //var ff = $(this).find("td").eq(0);
+         //            $(this).find("#check").attr("checked",false)
+         //ff.attr("checked", true);
+         //console.log("ff = " + ff);
+         if ($(this).find("#check").attr("checked", true)) {
+         console.log("allilyia");
+         //console.log("ff = " + ff);
+         }
+         });
+         */
+
+
+        var currentSurname = deleteCell.closest("tr").find("td").eq(2).html();
+        var currentName = deleteCell.closest("tr").find("td").eq(3).html();
+
+        //console.log("row = " + row.closest("tr").find("td").eq(2).html());
+
+        //var ff = row.closest("tr").find("td")[2];
+
+        //console.log("ff = " + ff);
+        //console.log(row.closest("tr").find("td")[2]);
+        //console.log(row.closest("tr").find("td")[3]);
+        var res = confirm("Вы действительно  хотите удалить контакт : " + currentSurname + " " + currentName);
+        if (res) {
+            deleteCell.closest("tr").remove();
+            reordersRow();
+        }
+    }
+
+    //var surname = "";
+    //var name = "";
+    //var phoneNumber = "";
+
 
     function checkDuplicateNumbers(phoneNumber) {
         var isHasDuplicates = false;
@@ -39,19 +126,31 @@ $(document).ready(function () {
 
     function checkInputFields() {
         //получаем значение полей
-        surname = $("#my-surname").val();
-        name = $("#my-name").val();
-        phoneNumber = $("#my-phone").val();
+        var surname = $("#my-surname").val();
+        var name = $("#my-name").val();
+        var phoneNumber = $("#my-phone").val();
         var valid = true;
 
         if (surname == "" || name == "" || phoneNumber == "") {
-            $(".inputField").css("border", "2px solid red");
+            //$(".inputField").css("border", "2px solid red");
+            $(".inputField").addClass("empty_field");
             surname = "";
             name = "";
             phoneNumber = "";
             valid = false;
-        } else {
-            $(".inputField").css("border", "2px solid black");
+        }
+        else {
+            $(".inputField").removeClass("empty_field");
+            //$("form").addClass(".inputField");
+            //$("input").addClass(".inputField input:focus");
+            //$(".inputField").css("border", "2px solid black");
+            //$(".mainForm input:focus").css("border-color", "#07a6e6");
+            //$(".inputField input:focus").css("border-color", "#07a6e6");
+
+            //outline: none;
+            //border-color: #07a6e6;
+            //border-radius: 10px;
+
         }
 
         $('b.err').remove();
@@ -62,50 +161,106 @@ $(document).ready(function () {
     }
 
     $('.addUser').click(function () {
-        surname = $("#my-surname").val();//GETTER
-        name = $("#my-name").val();//GETTER
-        phoneNumber = $("#my-phone").val();//GETTER
+        var surname = $("#my-surname").val();//GETTER
+        //surname = $("#my-surname").text();//GETTER
+        var name = $("#my-name").val();//GETTER
+        var phoneNumber = $("#my-phone").val();//GETTER
+
+        surname = $.trim(surname);
+        name = $.trim(name);
+        phoneNumber = $.trim(phoneNumber);
 
         console.log("surname = " + surname);
         console.log("name = " + name);
         console.log("phoneNumber = " + phoneNumber);
 
+        var newRow = $("<tr><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
+        //var cells = newRow.find("td");
+
+
+        newRow.find("td").eq(0).html("<span><input type='checkbox' id='check'/></span>");
+        newRow.find("td").eq(1).text(userNumber);
+        newRow.find("td").eq(2).text(surname);
+        newRow.find("td").eq(3).text(name);
+        newRow.find("td").eq(4).text(phoneNumber);
+        newRow.find("td").eq(5).html("<input type='button' value='&times;' class='removeUser'/>");
+
+        newRow.find("td").eq(5).click(function () {
+            confirmDelete($(this));
+        });
+
+
+        //cells.eq(1).text;
+        //cells.eq(2);
+        //cells.eq(3);
+        //cells.eq(4);
+        //cells.eq(5);
+
+
         if (checkInputFields()) {
-            var row = '<tr id=' + userNumber
-                + '><td><span><input type="checkbox" /></span></td><td>'
-                + userNumber + '</td><td>'
-                + surname + '</td><td>' + name + '</td><td>'
-                + phoneNumber
-                + '</td><td><input type="button" value="&times;" class="removeUser" /></td></tr>';
+            /*
+             var row = '<tr id=' + userNumber
+             + '><td><span><input type="checkbox" /></span></td><td>'
+             + userNumber + '</td><td>'
+             + surname + '</td><td>' + name + '</td><td>'
+             + phoneNumber
+             + '</td><td><input type="button" onclick ="confirmDelete($(this))" value="&times;" class="removeUser" /></td></tr>';
+
+             */
+
             //onclick ="confirmDelete($(this))"
             //user_id="' + userNumber + '"
             //text = $(row).text();
-            console.log("row = " + row);
+
+
+            console.log("row = " + newRow);
             var bool = checkDuplicateNumbers(phoneNumber);
             console.log("bool=" + bool);
             if (bool) {
                 $('div.errorMessage').append('<b class="err">Этот номер уже есть в списке!!!</b>');
+            } else if (!$.isNumeric(phoneNumber)) {
+                $('div.errorMessage').append('<b class="err">Некорректный телефон. В поле телефон нужно вводить цифры!!!</b>');
             } else {
-                $('.mainTable').append(row);
+
+                $('.mainTable').append(newRow);
+
+                //$('.mainTable').append(row);
 
                 $("#my-surname").val("");   //SETTER
                 $("#my-name").val("");      //SETTER
                 $("#my-phone").val("");     //SETTER
                 ++userNumber;
 
-                $(".removeUser").click(function () {
-                    //var res = confirm("Вы действительно хотите удалить этого пользователя?");
-                    //console.log("res=" + res);
+                //$(".mainTable").click(function () {
 
-                    //if (res) {
-                    //$(this).closest("tr").remove();
-                    //row.closest('tr').remove();
-                    //reordersRow();
-                    //return false;
-                    //}
-                    confirmDelete($(this));
-                    //$(this).closest("tr").remove();
-                });
+
+                //$(".mainTable").find(".removeUser").click(function () {
+                //var res = confirm("Вы действительно хотите удалить этого пользователя?");
+                //if (res) {
+                //$(this).closest("tr").remove();
+                //row.closest('tr').remove();
+                //reordersRow();
+                //}
+
+                //});
+                //});
+
+
+                //$(".removeUser").click(function () {
+
+
+                //var res = confirm("Вы действительно хотите удалить этого пользователя?");
+                //console.log("res=" + res);
+
+                //if (res) {
+                //$(this).closest("tr").remove();
+                //row.closest('tr').remove();
+                //reordersRow();
+                //return false;
+                //}
+                //confirmDelete($(this));
+                //$(this).closest("tr").remove();
+                //});
 
             }
 
