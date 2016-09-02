3bd4d5a54475ce43ef7f1bf366f64201c7997438
ogogoshka <xxxpiracy@gmail.com>
2016-09-02 23:28:45 +0600

    DONE

--- PhoneBook/src/js/phonebook.js
+++ PhoneBook/src/js/phonebook.js
@@ -1,10 +1,9 @@
-var userNumber = 1;
-
-
 $(document).ready(function () {
 
-    var reordersRow = function () {
-        var rows = $(".mainTable").find("tbody").find("tr");
+    var userNumber = 1;
+
+    var reorderRows = function () {
+        var rows = $(".main-table").find("tbody").find("tr");
         userNumber = rows.length + 1;
         $(rows).each(function (i) {
             $(this).find("td").eq(1).text(i + 1);
@@ -12,45 +11,39 @@ $(document).ready(function () {
     };
 
 
-    $(".ButtonForDeleteUser").on('click', function () {
+    $(".delete-user-from-table").on("click", function () {
         var checked = $("input:checkbox:checked").map(function () {
-
+            //var checked = $("#check").is(":checked").map(function () {
             return this.value;
         }).get();
 
-
-
         if (confirm("Вы действительно хотите удалить эти контакты?")) {
             $("#check:checked").parents("tr").remove();
-            reordersRow();
+            reorderRows();
         }
-
 
     });
 
     function confirmDelete(deleteCell) {
 
-
         var currentSurname = deleteCell.closest("tr").find("td").eq(2).html();
         var currentName = deleteCell.closest("tr").find("td").eq(3).html();
 
+        if (confirm("Вы действительно  хотите удалить контакт : " + currentSurname + " " + currentName)) {
 
-        var res = confirm("Вы действительно  хотите удалить контакт : " + currentSurname + " " + currentName);
-        if (res) {
             deleteCell.closest("tr").remove();
-            reordersRow();
+            reorderRows();
         }
     }
-
 
     function checkDuplicateNumbers(phoneNumber) {
         var isHasDuplicates = false;
 
-        var col = $(".mainTable").find("tbody").find("tr");
+        var phoneColumn = $(".main-table").find("tbody").find("tr");
         if (userNumber == 1) {
             return false;
         } else {
-            $(col).each(function () {
+            $(phoneColumn).each(function () {
                 if (phoneNumber == $(this).find("td").eq(4).text()) {
                     isHasDuplicates = true;
                 }
@@ -67,28 +60,26 @@ $(document).ready(function () {
         var valid = true;
 
         if (surname == "" || name == "" || phoneNumber == "") {
-            //$(".inputField").css("border", "2px solid red");
-            $(".inputField").addClass("empty_field");
+            $(".input-fields").addClass("empty-fields");
             surname = "";
             name = "";
             phoneNumber = "";
             valid = false;
         }
         else {
-            $(".inputField").removeClass("empty_field");
+            $(".input-fields").removeClass("empty-fields");
 
         }
 
-        $('b.err').remove();
+        $("b.err").remove();
         if (!valid) {
-            $('div.errorMessage').append('<b class="err">Нужно заполнить все поля</b>');
+            $("div.error-message").append("<b class='err'>Нужно заполнить все поля</b>");
         }
         return valid;
     }
 
-    $('.addUser').click(function () {
+    $(".add-user-to-table").click(function () {
         var surname = $("#my-surname").val();//GETTER
-        //surname = $("#my-surname").text();//GETTER
         var name = $("#my-name").val();//GETTER
         var phoneNumber = $("#my-phone").val();//GETTER
 
@@ -101,46 +92,31 @@ $(document).ready(function () {
         console.log("phoneNumber = " + phoneNumber);
 
         var newRow = $("<tr><td></td><td></td><td></td><td></td><td></td><td></td></tr>");
-        //var cells = newRow.find("td");
-
 
         newRow.find("td").eq(0).html("<span><input type='checkbox' id='check'/></span>");
         newRow.find("td").eq(1).text(userNumber);
         newRow.find("td").eq(2).text(surname);
         newRow.find("td").eq(3).text(name);
         newRow.find("td").eq(4).text(phoneNumber);
-        newRow.find("td").eq(5).html("<input type='button' value='&times;' class='removeUser'/>");
+        newRow.find("td").eq(5).html("<input type='button' value='&times;' class='remove-user-cell'/>");
 
         newRow.find("td").eq(5).click(function () {
             confirmDelete($(this));
         });
 
-
-
         if (checkInputFields()) {
-
-
-            console.log("row = " + newRow);
-            var bool = checkDuplicateNumbers(phoneNumber);
-            console.log("bool=" + bool);
-            if (bool) {
-                $('div.errorMessage').append('<b class="err">Этот номер уже есть в списке!!!</b>');
+            var isHasDuplicate = checkDuplicateNumbers(phoneNumber);
+            if (isHasDuplicate) {
+                $("div.error-message").append("<b class='err'>Этот номер уже есть в списке!!!</b>");
             } else if (!$.isNumeric(phoneNumber)) {
-                $('div.errorMessage').append('<b class="err">Некорректный телефон. В поле телефон нужно вводить цифры!!!</b>');
+                $("div.error-message").append("<b class='err'>Некорректный телефон. В поле телефон нужно вводить цифры!!!</b>");
             } else {
-
-                $('.mainTable').append(newRow);
-
-                //$('.mainTable').append(row);
-
+                $(".main-table").append(newRow);
                 $("#my-surname").val("");   //SETTER
                 $("#my-name").val("");      //SETTER
                 $("#my-phone").val("");     //SETTER
                 ++userNumber;
-
             }
-
-
         } else {
             surname = "";
             name = "";
@@ -148,17 +124,49 @@ $(document).ready(function () {
         }
     });
 
-    var table = $('.mainTable');
+    var table = $(".main-table");
     table
-        .on('change', '> tbody input:checkbox', function () {
-            $(this).closest('span').toggleClass('checked', $(this).is(':checked'));
+        .on("change", "> tbody input:checkbox", function () {
+            $(this).closest("span").toggleClass("checked", $(this).is(":checked"));
         })
-        .on('change', '#all', function () {
-            $('> tbody input:checkbox', table).prop('checked', $(this).is(':checked')).trigger('change');
+        .on("change", "all", function () {
+            $("> tbody input:checkbox", table).prop("checked", $(this).is(":checked")).trigger("change");
         });
 
-});
+    $(".apply-filter-button").click(function () {
+        var filter = $(".filter-field").val().toLowerCase();
+        filter = $.trim(filter);
 
-$(document).ready(function () {
+        $(".main-table").find("tbody").find("tr").each(function () {
+            if (filter == "") {
+                $(".main-table").find("tbody").find("tr").each(function () {
+                    $('tbody tr').removeClass('visible').show().addClass('visible');
+                    $(this).show();
+                });
+            } else {
+
+                if ($(this).find("td:nth-child(3)").text().search(filter) < 0) {
+                    if ($(this).find("td:nth-child(4)").text().search(filter) < 0) {
+                        if ($(this).find("td:nth-child(5)").text().search(filter) < 0) {
+                            $(this).hide();
+                        }
+                    }
+                } else {
+                    $(this).show()
+                }
+
+            }
+
+        });
+
+    });
+
+    $(".reset-filter-button").click(function () {
+        $(".filter-field").val("");
+        $(".main-table").find("thead").find("tr").show();
+        $(".main-table").find("tbody").find("tr").each(function () {
+            $(this).show();
+        });
+    });
 
 });
