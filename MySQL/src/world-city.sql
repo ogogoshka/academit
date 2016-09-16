14bba27ea932d6fc274574e94f3da76f2cbc18a0
ogogoshka <xxxpiracy@gmail.com>
2016-09-16 22:39:39 +0600

    DONE

--- MySQL/src/world-city.sql
+++ MySQL/src/world-city.sql
@@ -1,7 +1,7 @@
 USE world;
-SELECT id, name, district FROM city;
-SELECT * FROM city where Code ='RUS';
-SELECT * FROM city WHERE CountryCode = 'GRC' OR CountryCode = 'ESP' OR CountryCode = 'PRT' ORDER BY name DESC
+SELECT name, district FROM city;
+SELECT * FROM city WHERE CountryCode ='RUS' ORDER BY name ASC;
+SELECT * FROM city WHERE CountryCode IN ('GRC', 'ESP', 'PRT') ORDER BY name DESC;
 SELECT *  FROM city WHERE Population >= 300000 AND Population <= 500000;
 SELECT *  FROM city WHERE name LIKE 'a%';
 SELECT *  FROM city WHERE name LIKE '%a%';
